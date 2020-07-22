8b0a3d800638c66876a6df09ea2224c3d052f363
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-07-22 20:11:58 +0200

    Initial Upload

---
+++ ttt2_snuggle_snuggle/lua/lang/deutsch/snugglestruggle.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference( "deutsch" )
+L[ "ttt2_snugglestruggle_name" ] = "Kuschelprobleme"
+L[ "ttt2_snugglestruggle_desc" ] = "Vergewaltige die Bitches ... oder die Jungs. Habe Spaß!"
+L[ "ttt2_sungglestruggle_help1" ] = "{primaryfire} spaß haben"
+L[ "ttt2_sungglestruggle_help2" ] = "{secondaryfire} verspotten"
