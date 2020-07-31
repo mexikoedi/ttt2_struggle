0b064e51ea8f191b76527e113be3a874ad6f0e06
DennisWolfgang <31799620+DennisWolfgang@users.noreply.github.com>
2020-07-31 17:45:34 -0400

    Create snugglestruggle.lua

---
+++ ttt2_snuggle_snuggle/lua/lang/español/snugglestruggle.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference( "español" )
+L[ "ttt2_snugglestruggle_name" ] = "Echar un polvo"
+L[ "ttt2_snugglestruggle_desc" ] = "¡Cógete a las perras de los innocentes!
+L[ "ttt2_sungglestruggle_help1" ] = "{primaryfire} para pasar un buen rato"
+L[ "ttt2_sungglestruggle_help2" ] = "{secondaryfire} para hacer una burla"
