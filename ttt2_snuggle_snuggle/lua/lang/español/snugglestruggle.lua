8567e305e1740fe7fffdc06dad8d0138b9cef188
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-08-02 16:17:58 +0200

    Delete snugglestruggle.lua

--- ttt2_snuggle_snuggle/lua/lang/español/snugglestruggle.lua
+++
@@ -1,5 +0,0 @@
-L = LANG.GetLanguageTableReference( "español" )
-L[ "ttt2_snugglestruggle_name" ] = "Ñiqui ñiqui"
-L[ "ttt2_snugglestruggle_desc" ] = "¡Fóllate a las perras de los innocentes!"
-L[ "ttt2_sungglestruggle_help1" ] = "{primaryfire} para pasar un buen rato"
-L[ "ttt2_sungglestruggle_help2" ] = "{secondaryfire} para hacer una burla"
