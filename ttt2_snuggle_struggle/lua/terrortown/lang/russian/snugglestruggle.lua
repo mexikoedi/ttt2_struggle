52b068c36c107fb7c26ee9cc41a41b4e9ced4be2
Blueberryy <igory.ygr200@gmail.com>
2020-10-02 19:11:51 +0500

    Create snugglestruggle.lua

---
+++ ttt2_snuggle_struggle/lua/terrortown/lang/russian/snugglestruggle.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference( "Русский" )
+L[ "ttt2_snugglestruggle_name" ] = "Обнимашки"
+L[ "ttt2_snugglestruggle_desc" ] = "Насилуйте сучек... или парней. Веселитесь!"
+L[ "ttt2_sungglestruggle_help1" ] = "{primaryfire}, чтобы веселиться"
+L[ "ttt2_sungglestruggle_help2" ] = "{secondaryfire} для насмешки"
