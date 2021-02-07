13d7784cb3c1aa36ffbd40dfbf84ae75cfd15111
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-07 14:34:40 +0100

    updated to new ttt2 language identifiers

--- ttt2_snuggle_struggle/lua/terrortown/lang/spanish/snugglestruggle.lua
+++ ttt2_snuggle_struggle/lua/terrortown/lang/spanish/snugglestruggle.lua
@@ -1,4 +1,4 @@
-L = LANG.GetLanguageTableReference( "español" )
+L = LANG.GetLanguageTableReference( "es" )
 L[ "ttt2_snugglestruggle_name" ] = "Ñiqui ñiqui"
 L[ "ttt2_snugglestruggle_desc" ] = "¡Fóllate a las perras de los innocentes!"
 L[ "ttt2_sungglestruggle_help1" ] = "{primaryfire} para pasar un buen rato"
