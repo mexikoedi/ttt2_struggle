245e8c2e73632e0dde49b5dc1e16648c9c4d4ea3
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:23:03 +0100

    formatted code + updated folder name

---
+++ ttt2_snuggle_struggle/lua/terrortown/lang/en/snugglestruggle.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("en")
+L["ttt2_snugglestruggle_name"] = "Snuggle Struggle"
+L["ttt2_snugglestruggle_desc"] = "Rape Dem' Bitches... or guys. Have Fun!"
+L["ttt2_sungglestruggle_help1"] = "{primaryfire} to have fun"
+L["ttt2_sungglestruggle_help2"] = "{secondaryfire} to taunt"
