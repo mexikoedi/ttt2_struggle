964f85a959ba1dad7c2b68a9b38d5e298658434f
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:22:12 +0100

    formatted code

--- ttt2_snuggle_struggle/lua/autorun/shared.lua
+++ ttt2_snuggle_struggle/lua/autorun/shared.lua
@@ -1,7 +1,7 @@
 if CLIENT then
-    // Use string or string.format("%.f",<steamid64>) 
-    // addon dev emblem in scoreboard
-    hook.Add( "TTT2FinishedLoading" , "TTT2RegistermexikoediAddonDev" , function( )
-        AddTTT2AddonDev( "76561198279816989" )
-    end )
+    -- Use string or string.format("%.f",<steamid64>) 
+    -- addon dev emblem in scoreboard
+    hook.Add("TTT2FinishedLoading", "TTT2RegistermexikoediAddonDev", function()
+        AddTTT2AddonDev("76561198279816989")
+    end)
 end
