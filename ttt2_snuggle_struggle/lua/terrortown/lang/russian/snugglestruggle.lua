7c824e6513808e9628529d21c7b00d9a4c61f7fa
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:23:31 +0100

    formatted code + updated folder name

--- ttt2_snuggle_struggle/lua/terrortown/lang/russian/snugglestruggle.lua
+++
@@ -1,5 +0,0 @@
-L = LANG.GetLanguageTableReference( "ru" )
-L[ "ttt2_snugglestruggle_name" ] = "Обнимашки"
-L[ "ttt2_snugglestruggle_desc" ] = "Насилуйте сучек... или парней. Веселитесь!"
-L[ "ttt2_sungglestruggle_help1" ] = "{primaryfire}, чтобы веселиться"
-L[ "ttt2_sungglestruggle_help2" ] = "{secondaryfire} для насмешки"
