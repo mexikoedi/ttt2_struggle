9b5031244939c1b8def8bded835950d243aa4aa7
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-04-14 22:09:03 +0200

    added new info

--- ttt2_snuggle_struggle/lua/terrortown/lang/de/snugglestruggle.lua
+++ ttt2_snuggle_struggle/lua/terrortown/lang/de/snugglestruggle.lua
@@ -1,5 +1,5 @@
 L = LANG.GetLanguageTableReference("de")
 L["ttt2_snugglestruggle_name"] = "Kuschelprobleme"
-L["ttt2_snugglestruggle_desc"] = "Vergewaltige die Bitches ... oder die Jungs. Habe Spaß!"
+L["ttt2_snugglestruggle_desc"] = "Vergewaltige die Bitches ... oder die Jungs. Habe Spaß! Nicht nutzbar wenn die Tarnung gekauft wurde!"
 L["ttt2_sungglestruggle_help1"] = "{primaryfire} spaß haben"
 L["ttt2_sungglestruggle_help2"] = "{secondaryfire} verspotten"
