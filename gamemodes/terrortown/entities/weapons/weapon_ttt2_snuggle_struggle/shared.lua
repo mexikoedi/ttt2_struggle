4850546cf7542f34deefa68c28bfb4b2e07cfc82
mexikoedi <mexikoedi@outlook.com>
2023-07-27 18:48:18 +0200

    formatted code + added/updated comments

--- gamemodes/terrortown/entities/weapons/weapon_ttt2_snuggle_struggle/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt2_snuggle_struggle/shared.lua
@@ -7,12 +7,9 @@ end
 SWEP.Base = "weapon_tttbase"
 SWEP.Kind = WEAPON_EQUIP1
 SWEP.InLoadoutFor = nil
-
 SWEP.CanBuy = {ROLE_TRAITOR}
-
 SWEP.LimitedStock = true
 SWEP.Icon = "vgui/ttt/weapon_snuggle_struggle"
-
 SWEP.EquipMenuData = {
     type = "item_weapon",
     name = "ttt2_snugglestruggle_name",
@@ -50,26 +47,17 @@ SWEP.Secondary.Delay = 3
 attackerweapons = {}
 victimweapons = {}
 SWEP.ViewModel = Model("models/weapons/v_hands.mdl")
-SWEP.RapeLength = GetConVar("ttt2_snuggle_struggle_length"):GetInt()
+SWEP.SnuggleLength = GetConVar("ttt2_snuggle_struggle_length"):GetInt()
 SWEP.SoundDelay = 1.5
 local ThrustVelocity = 750
-
 local boneNames = {"ValveBiped.Bip01_Spine2", "ValveBiped.Bip01_R_UpperArm", "ValveBiped.Bip01_L_UpperArm", "ValveBiped.Bip01_L_Forearm", "ValveBiped.Bip01_L_Hand", "ValveBiped.Bip01_R_Forearm", "ValveBiped.Bip01_R_Hand", "ValveBiped.Bip01_R_Thigh", "ValveBiped.Bip01_R_Calf", "ValveBiped.Bip01_Head1", "ValveBiped.Bip01_L_Thigh", "ValveBiped.Bip01_L_Calf", "ValveBiped.Bip01_L_Foot", "ValveBiped.Bip01_R_Foot",}
-
 local victimPos = {Vector(4.1165161132813, 0.33807373046875, 24.5458984375), Vector(10.886047363281, -6.7128601074219, 17.712890625), Vector(14.32470703125, 6.6110534667969, 24.662109375), Vector(10.561950683594, 11.863800048828, 14.9169921875), Vector(14.15673828125, 11.935821533203, 4.0126953125), Vector(8.0779418945313, -10.698425292969, 7.0859375), Vector(17.083557128906, -6.5664367675781, 1.28515625), Vector(-6.3666381835938, -3.5222778320313, 22.1142578125), Vector(-5.1080932617188, -3.3052978515625, 4.3046875), Vector(18.328247070313, -0.20745849609375, 18.6533203125), Vector(-7.3578491210938, 4.1403503417969, 22.0517578125), Vector(-6.7325439453125, 3.0444641113281, 4.2421875), Vector(-23.113708496094, 2.7197570800781, 6.40234375), Vector(-21.197875976563, -3.1515197753906, 6.390625),}
-
 local victimAng = {Angle(13.763027191162, -7.126654624939, 297.44543457031), Angle(65.35457611084, -125.17009735107, 116.02203369141), Angle(56.451721191406, 125.61678314209, 68.999862670898), Angle(71.750823974609, 1.1475394964218, 309.23059082031), Angle(10.08162021637, 20.952842712402, 89.150527954102), Angle(30.348388671875, 24.646505355835, 257.76071166992), Angle(-31.068134307861, 5.5770988464355, 100.95864105225), Angle(85.568023681641, -10.062316894531, 259.72805786133), Angle(-11.049569129944, 179.28791809082, 92.02823638916), Angle(36.883769989014, -10.713672637939, 95.383735656738), Angle(87.360504150391, -50.038749694824, 219.5774230957), Angle(-7.4636454582214, -178.8634185791, 90.854110717773), Angle(20.328042984009, 175.31163024902, 84.239585876465), Angle(20.698318481445, -177.09216308594, 94.63240814209),}
-
 local attackerPos = {Vector(-0.65728759765625, 0.377197265625, 30.7626953125), Vector(9.2244873046875, -3.9486083984375, 35.96484375), Vector(6.1197509765625, 10.176849365234, 30.6552734375), Vector(8.9208984375, 12.130462646484, 20.0087890625), Vector(11.781127929688, 14.134094238281, 9.0712890625), Vector(10.643859863281, -13.228057861328, 29.0009765625), Vector(15.782470703125, -6.8009948730469, 20.9951171875), Vector(-9.7853393554688, -4.5965576171875, 24.3857421875), Vector(-11.373352050781, -7.4108276367188, 6.8203125), Vector(13.504028320313, 4.9801635742188, 34.6708984375), Vector(-11.517456054688, 2.9996948242188, 25.9482421875), Vector(-14.434143066406, 11.57861328125, 10.537109375), Vector(-30.881225585938, 10.939605712891, 9.0634765625), Vector(-26.714416503906, -13.074645996094, 9.19921875),}
-
 local attackerAng = {Angle(-23.909204483032, 21.916522979736, 247.47807312012), Angle(36.469985961914, -81.487747192383, 121.61176300049), Angle(72.325134277344, 35.100917816162, 310.2248840332), Angle(72.292373657227, 35.012317657471, 310.07629394531), Angle(53.682888031006, 77.813186645508, 63.455425262451), Angle(44.2184715271, 51.356468200684, 233.86199951172), Angle(18.646379470825, 13.987821578979, 129.42607116699), Angle(79.875244140625, -120.97535705566, 128.95693969727), Angle(-8.2740859985352, -159.73634338379, 95.78791809082), Angle(4.1483745574951, 25.958532333374, 65.960624694824), Angle(59.834487915039, 108.70742797852, 10.802012443542), Angle(5.1143140792847, -177.7751159668, 61.338542938232), Angle(46.266540527344, 113.92001342773, 15.213181495667), Angle(49.310661315918, -157.73908996582, 96.905143737793),}
-
 local sounds = {"player/crit_death1.wav", "player/crit_death2.wav", "player/crit_death3.wav", "player/crit_death4.wav", "player/crit_death5.wav", "bot/come_to_papa.wav", "bot/im_pinned_down.wav", "bot/oh_man.wav", "bot/yesss.wav", "bot/pain4", "bot/pain5", "bot/pain8", "bot/pain9", "bot/pain10", "bot/stop_it.wav", "bot/help.wav", "bot/i_could_use_some_help.wav", "bot/i_could_use_some_help_over_here.wav", "bot/they_got_me_pinned_down_here.wav", "bot/this_is_my_house.wav", "bot/need_help.wav", "bot/i_am_dangerous.wav", "bot/yikes.wav", "noo.wav", "bot/whos_the_man.wav", "bot/hang_on_im_coming.wav", "hostage/hpain/hpain1.wav", "hostage/hpain/hpain2.wav", "hostage/hpain/hpain3.wav", "hostage/hpain/hpain4.wav", "hostage/hpain/hpain5.wav", "hostage/hpain/hpain6.wav", "vo/k_lab/al_youcoming.wav", "vo/k_lab/kl_ahhhh.wav", "noice.wav",}
-
 local sounds2 = {"bot/where_are_you_hiding.wav", "vo/NovaProspekt/al_whereareyou03.wav", "vo/Citadel/al_wonderwhere.wav",}
-
 local sounds3 = {"physics/body/body_medium_break2.wav", "physics/body/body_medium_break3.wav", "physics/body/body_medium_break4.wav", "physics/body/body_medium_impact_hard1.wav", "physics/body/body_medium_impact_hard2.wav", "physics/body/body_medium_impact_hard3.wav", "physics/body/body_medium_impact_hard4.wav", "physics/body/body_medium_impact_hard5.wav", "physics/body/body_medium_impact_hard6.wav",}
-
 function SWEP:Initialize()
     if CLIENT then
         self:AddHUDHelp("ttt2_sungglestruggle_help1", "ttt2_sungglestruggle_help2", true)
@@ -77,6 +65,7 @@ function SWEP:Initialize()
 end
 
 if SERVER then
+    -- damage/inflictor code
     local function InstantDamage(ply, damage, attacker, inflictor)
         local dmg = DamageInfo()
         dmg:SetDamage(damage or 500)
@@ -84,7 +73,6 @@ if SERVER then
         dmg:SetDamageForce(ply:GetAimVector())
         dmg:SetDamagePosition(ply:GetPos())
         dmg:SetDamageType(DMG_GENERIC)
-
         if inflictor then
             dmg:SetInflictor(inflictor)
         end
@@ -93,18 +81,17 @@ if SERVER then
     end
 
     function SWEP:PrimaryAttack()
-        -- set up postioning stuff and owner/victim checks
+        -- set up positioning stuff and owner/victim checks
         local owner = self:GetOwner()
-
         if GetRoundState() ~= ROUND_ACTIVE or GetRoundState() == ROUND_PREP or GetRoundState() == ROUND_WAIT or GetRoundState() == ROUND_POST then
             owner:ChatPrint("Round is not active, you can't use this weapon!")
 
             return
         end
 
+        -- disable usage if disguiser is already bought
         local victim = owner:GetEyeTrace().Entity
         if not IsValid(victim) or victim:IsNPC() or not victim:IsPlayer() or not victim:IsTerror() or not victim:IsActive() then return end
-
         if owner:HasEquipmentItem("item_ttt_disguiser") then
             owner:ChatPrint("You can't use this weapon with a disguiser!")
 
@@ -117,18 +104,15 @@ if SERVER then
         local traceda = {}
         traceda.start = positionBase
         traceda.endpos = positionBase - Vector(0, 0, 1000)
-
         traceda.filter = {victim, owner}
-
         local trace = util.TraceLine(traceda)
         positionBase = trace.HitPos or positionBase
         if positionVictim:Distance(positionOwner) > self.Primary.Distance then return end
-
         if GetConVar("ttt2_snuggle_struggle_primary_sound"):GetBool() then
             owner:EmitSound(self.Primary.Sound)
         end
 
-        -- set owner to god mode with no movement + hide his name/give item + position him and set victim to spectator camera 
+        -- set owner to godmode with no movement + hide his name/give item + position him and set victim to spectator camera 
         owner:GodEnable()
         owner:SetJumpPower(1)
         owner:SetCrouchedWalkSpeed(0.1)
@@ -148,11 +132,9 @@ if SERVER then
         victimRagdoll:SetPos(positionVictim)
         victimRagdoll:Spawn()
         victimRagdoll:Activate()
-
         for i = 1, victimRagdoll:GetPhysicsObjectCount() - 1 do
             local boneName = boneNames[i]
             local boneId = victimRagdoll:LookupBone(boneName)
-
             if boneId ~= nil then
                 local physId = victimRagdoll:TranslateBoneToPhysBone(boneId)
                 local phys = victimRagdoll:GetPhysicsObjectNum(physId)
@@ -169,11 +151,9 @@ if SERVER then
         ownerRagdoll:SetPos(positionOwner)
         ownerRagdoll:Spawn()
         ownerRagdoll:Activate()
-
         for i = 1, ownerRagdoll:GetPhysicsObjectCount() - 1 do
             local boneName = boneNames[i]
             local boneId = ownerRagdoll:LookupBone(boneName)
-
             if boneId ~= nil then
                 local physId = ownerRagdoll:TranslateBoneToPhysBone(boneId)
                 local phys = ownerRagdoll:GetPhysicsObjectNum(physId)
@@ -182,7 +162,6 @@ if SERVER then
                 phys:SetAngles(attackerAng[i])
                 phys:EnableCollisions(false)
                 phys:EnableMotion(true)
-
                 if i == 2 or i == 5 or i == 7 or i == 10 or i == 13 or i == 14 then
                     phys:EnableMotion(false)
                 end
@@ -192,88 +171,99 @@ if SERVER then
         -- spectate each other and play random sounds + make name appear again
         owner:SpectateEntity(ownerRagdoll)
         victim:SpectateEntity(ownerRagdoll)
-        local thrustTimerString = "RapeThrust_" .. (owner:SteamID64() or "SINGLEPLAYER")
+        local thrustTimerString = "SnuggleThrust_" .. (owner:SteamID64() or "SINGLEPLAYER")
         local boneName = boneNames[11]
         local boneId = ownerRagdoll:LookupBone(boneName)
-
         if boneId ~= nil then
             local physId = ownerRagdoll:TranslateBoneToPhysBone(boneId)
             local phys = ownerRagdoll:GetPhysicsObjectNum(physId)
-
             if IsValid(phys) then
                 phys:SetVelocity(Vector(0, 0, 100000))
-
-                timer.Create(thrustTimerString, 0.3, 0, function()
-                    phys:SetVelocity(Vector(0, 0, ThrustVelocity))
-
-                    if GetConVar("ttt2_snuggle_struggle_animation_sound"):GetBool() and IsValid(victimRagdoll) and math.random(5) == 3 then
-                        victimRagdoll:EmitSound(sounds3[math.random(#sounds3)])
+                timer.Create(
+                    thrustTimerString,
+                    0.3,
+                    0,
+                    function()
+                        phys:SetVelocity(Vector(0, 0, ThrustVelocity))
+                        if GetConVar("ttt2_snuggle_struggle_animation_sound"):GetBool() and IsValid(victimRagdoll) and math.random(5) == 3 then
+                            victimRagdoll:EmitSound(sounds3[math.random(#sounds3)])
+                        end
                     end
-                end)
+                )
             end
         end
 
-        local soundTimerString = "EmitRapeSounds_" .. (owner:SteamID64() or "SINGLEPLAYER")
-
+        local soundTimerString = "EmitSnuggleSounds_" .. (owner:SteamID64() or "SINGLEPLAYER")
         if GetConVar("ttt2_snuggle_struggle_animation_sound"):GetBool() then
-            timer.Create(soundTimerString, self.SoundDelay, 0, function()
-                if not victimRagdoll:IsValid() then return end
-                victimRagdoll:EmitSound(sounds[math.random(#sounds)])
-            end)
+            timer.Create(
+                soundTimerString,
+                self.SoundDelay,
+                0,
+                function()
+                    if not victimRagdoll:IsValid() then return end
+                    victimRagdoll:EmitSound(sounds[math.random(#sounds)])
+                end
+            )
         end
 
-        timer.Create("itemremoval", self.RapeLength - 0.5, 0, function()
-            owner:ConCommand("ttt_toggle_disguise")
-        end)
+        timer.Create(
+            "itemremoval",
+            self.SnuggleLength - 0.5,
+            0,
+            function()
+                owner:ConCommand("ttt_toggle_disguise")
+            end
+        )
 
         -- check health/give health + positioning of owner/victim and letting the owner move again with no godmode + remove item
-        timer.Simple(self.RapeLength, function()
-            if IsValid(owner) then
-                if owner:Health() < 100 then
-                    owner:SetHealth(100)
+        timer.Simple(
+            self.SnuggleLength,
+            function()
+                if IsValid(owner) then
+                    if owner:Health() < 100 then
+                        owner:SetHealth(100)
+                    end
+
+                    owner:SetPos(positionOwner)
+                    owner:RemoveItem("item_ttt_disguiser")
+                    owner:SetJumpPower(160)
+                    owner:SetCrouchedWalkSpeed(0.3)
+                    owner:SetRunSpeed(220)
+                    owner:SetWalkSpeed(220)
+                    owner:SetMaxSpeed(220)
+                    owner:GodDisable()
+                    -- give loadout of owner
+                    owner:RestoreCachedWeapons()
+                    -- remove weapon and select last weapon
+                    owner:StripWeapon("weapon_ttt2_snuggle_struggle")
+                    RunConsoleCommand("lastinv")
                 end
 
-                owner:SetPos(positionOwner)
-                owner:RemoveItem("item_ttt_disguiser")
-                owner:SetJumpPower(160)
-                owner:SetCrouchedWalkSpeed(0.3)
-                owner:SetRunSpeed(220)
-                owner:SetWalkSpeed(220)
-                owner:SetMaxSpeed(220)
-                owner:GodDisable()
-                -- give loadout of owner
-                owner:RestoreCachedWeapons()
-                -- remove weapon and select last weapon
-                owner:StripWeapon("weapon_ttt2_snuggle_struggle")
-                RunConsoleCommand("lastinv")
-            end
+                if IsValid(victim) then
+                    victim:UnSpectate()
+                    victim:Spawn()
+                    victim:SetPos(positionVictim)
+                    -- give loadout of victim
+                    victim:RestoreCachedWeapons()
+                    -- create damage
+                    InstantDamage(victim, 500, owner, ents.Create("weapon_ttt2_snuggle_struggle"))
+                end
 
-            if IsValid(victim) then
-                victim:UnSpectate()
-                victim:Spawn()
-                victim:SetPos(positionVictim)
-                -- give loadout of victim
-                victim:RestoreCachedWeapons()
-                -- create damage
-                InstantDamage(victim, 500, owner, ents.Create("weapon_ttt2_snuggle_struggle"))
+                -- removing ragdolls and timers
+                SafeRemoveEntity(victimRagdoll)
+                SafeRemoveEntity(ownerRagdoll)
+                timer.Remove(soundTimerString)
+                timer.Remove(thrustTimerString)
+                timer.Remove("itemremoval")
             end
-
-            -- removing ragdolls and timers
-            SafeRemoveEntity(victimRagdoll)
-            SafeRemoveEntity(ownerRagdoll)
-            timer.Remove(soundTimerString)
-            timer.Remove(thrustTimerString)
-            timer.Remove("itemremoval")
-        end)
+        )
     end
 
     SWEP.NextSecondaryAttack = 0
-
     function SWEP:SecondaryAttack()
         if self.NextSecondaryAttack > CurTime() then return end
         self.NextSecondaryAttack = CurTime() + self.Secondary.Delay
         local owner = self:GetOwner()
-
         -- set up random sounds
         if GetConVar("ttt2_snuggle_struggle_secondary_sound"):GetBool() then
             owner:EmitSound(sounds2[math.random(#sounds2)])
