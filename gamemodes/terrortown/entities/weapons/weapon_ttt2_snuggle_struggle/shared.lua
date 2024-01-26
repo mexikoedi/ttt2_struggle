e15962eb2b3f79bc0a1e786a76093fe64f94ad7f
mexikoedi <mexikoedi@outlook.com>
2024-01-26 19:55:12 +0100

    moved settings (server addons -> equipment menu)

--- gamemodes/terrortown/entities/weapons/weapon_ttt2_snuggle_struggle/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt2_snuggle_struggle/shared.lua
@@ -230,3 +230,31 @@ if SERVER then
         if GetConVar("ttt2_snuggle_struggle_secondary_sound"):GetBool() then owner:EmitSound(sounds2[math.random(#sounds2)]) end
     end
 end
+
+if CLIENT then
+    function SWEP:AddToSettingsMenu(parent)
+        local form = vgui.CreateTTT2Form(parent, "header_equipment_additional")
+        form:MakeCheckBox({
+            serverConvar = "ttt2_snuggle_struggle_primary_sound",
+            label = "label_snuggle_struggle_primary_sound"
+        })
+
+        form:MakeCheckBox({
+            serverConvar = "ttt2_snuggle_struggle_secondary_sound",
+            label = "label_snuggle_struggle_secondary_sound"
+        })
+
+        form:MakeCheckBox({
+            serverConvar = "ttt2_snuggle_struggle_animation_sound",
+            label = "label_snuggle_struggle_animation_sound"
+        })
+
+        form:MakeSlider({
+            serverConvar = "ttt2_snuggle_struggle_length",
+            label = "label_snuggle_struggle_length",
+            min = 0,
+            max = 30,
+            decimal = 0
+        })
+    end
+end
